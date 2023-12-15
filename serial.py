76a8fe0f4880ea40e9fd4d617dd04403d4ae872e
Panos <panoslin@foxmail.com>
2023-12-15 14:51:35 -0500

    modify: comment and variable naming

--- serial.py
+++ serial.py
@@ -3,36 +3,42 @@
 # Created by panos on 12/10/23
 # IDE: PyCharm
 class Solution:
+    """
+    An implementation of the Needleman-Wunsch algorithm
+    """
+
     def minDistance(self, word1: str, word2: str) -> int:
         n, m = len(word1), len(word2)
-        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        score_matrix = [[0] * (m + 1) for _ in range(n + 1)]
         for i in range(1, n + 1):
-            dp[i][0] = i
+            score_matrix[i][0] = i
 
         for j in range(1, m + 1):
-            dp[0][j] = j
+            score_matrix[0][j] = j
 
         for i in range(1, n + 1):
             for j in range(1, m + 1):
                 if word1[i - 1] == word2[j - 1]:
                     # equal to case when both chars not presented
-                    dp[i][j] = dp[i - 1][j - 1]
+                    score_matrix[i][j] = score_matrix[i - 1][j - 1]
                 else:
-                    dp[i][j] = 1 + min(
+                    score_matrix[i][j] = 1 + min(
                         # insert
-                        dp[i - 1][j],
+                        score_matrix[i - 1][j],
                         # delete
-                        dp[i][j - 1],
+                        score_matrix[i][j - 1],
                         # replace
-                        dp[i - 1][j - 1],
+                        score_matrix[i - 1][j - 1],
                     )
-        return dp[-1][-1]
+        return score_matrix[-1][-1]
 
 
 if __name__ == '__main__':
     from testcases import testcases
+    import time
 
     for s1, s2, ans in testcases:
+        start_time = time.time()
         _ = Solution().minDistance(s1, s2)
         print(_)
         try:
@@ -40,3 +46,10 @@ if __name__ == '__main__':
         except AssertionError:
             print(s1, s2)
             raise AssertionError(f"The answer for {s1=} and {s2=} is {ans=}, but got {_=} instead")
+
+        print(
+            f'\nFinish with result {_}\n'
+            f'time taken to process m={len(s1)}xn={len(s2)} '
+            f'{time.time() - start_time}\n'
+            f'*****************************************************************************************************'
+        )
